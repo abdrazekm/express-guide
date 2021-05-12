3aec609a330a103255f7e96ada6332e989f6637c
abdrazek <abdrazek_m@yahoo.com>
2021-05-12 07:49:27 +0300

    Html

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
+const rootDir = require('../util/path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('In third mw');
-    res.send('default route');
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
